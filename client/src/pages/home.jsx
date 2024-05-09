16fa2538b551cc1716f7eeb6e4cf2a950bbf8df2
Maanav Nair <maanavnair20042@gmail.com>
2024-05-09 12:58:00 +0530

    calling api in frontend

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/navbar'
 
 const Home = () => {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await fetch('http://localhost:3000/api/video', {
+        method: 'GET',
+        credentials: 'include'
+      });
+
+      const data = await res.json();
+      setVideos(data.videos);
+      console.log(data);
+      console.log(videos)
+    }
+
+    fetchVideos();
+  }, []);
+
   return (
-    <div>
+    <div className='px-5'>
+      {
+        videos.map((video) => (
+          <div key={video.id}>
+            <img src={video.thumbnailLink} />
+            <h1>{video.title}</h1>
+            <p>Views: {video.views}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
